cb3e7d370532805c9c30396c48a7c5ed3e3a2eb3
ilmaruk <ruggero.ferretti@gmail.com>
2025-08-02 12:46:04 +0200

    Fixed culture selection. Added get_culture() and get_gender() methods to name class.

--- examples/example.cpp
+++ examples/example.cpp
@@ -1,10 +1,80 @@
 #include "../dasmig/namegen.hpp"
 #include <iostream>
 
+using ng = dasmig::ng;
+
+std::wostream &operator<<(std::wostream &wos, const ng::culture &c)
+{
+    switch (c)
+    {
+    case ng::culture::american:
+        return wos << "American";
+    case ng::culture::argentinian:
+        return wos << "Argentinian";
+    case ng::culture::australian:
+        return wos << "Australian";
+    case ng::culture::brazilian:
+        return wos << "Brazilian";
+    case ng::culture::british:
+        return wos << "British";
+    case ng::culture::bulgarian:
+        return wos << "Bulgarian";
+    case ng::culture::canadian:
+        return wos << "Canadian";
+    case ng::culture::chinese:
+        return wos << "Chinese";
+    case ng::culture::danish:
+        return wos << "Danish";
+    case ng::culture::finnish:
+        return wos << "Finnish";
+    case ng::culture::french:
+        return wos << "French";
+    case ng::culture::german:
+        return wos << "German";
+    case ng::culture::kazakh:
+        return wos << "Kazakh";
+    case ng::culture::mexican:
+        return wos << "Mexican";
+    case ng::culture::norwegian:
+        return wos << "Norwegian";
+    case ng::culture::polish:
+        return wos << "Polish";
+    case ng::culture::portuguese:
+        return wos << "Portuguese";
+    case ng::culture::russian:
+        return wos << "Russian";
+    case ng::culture::spanish:
+        return wos << "Spanish";
+    case ng::culture::swedish:
+        return wos << "Swedish";
+    case ng::culture::turkish:
+        return wos << "Turkish";
+    case ng::culture::ukrainian:
+        return wos << "Ukrainian";
+    default:
+        return wos << "Unknown culture";
+    }
+}
+
+std::wostream &operator<<(std::wostream &wos, const ng::gender &g)
+{
+    switch (g)
+    {
+    case ng::gender::m:
+        return wos << "male";
+    case ng::gender::f:
+        return wos << "female";
+    default:
+        return wos << "Unknown gender";
+    }
+}
 
 int main()
 {
-    std::wcout << dasmig::ng::instance().get_name().append_surname()
+    ng::instance().load("../resources");
+    auto name = ng::instance().get_name().append_surname();
+    std::wcout << name.get_culture() << L"|" << name.get_gender()
+               << L": " << name
                << std::endl;
 
     return 0;
